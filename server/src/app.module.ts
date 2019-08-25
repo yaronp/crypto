6a048ad06db245ab0f48e48f19e7a32ea42ac48f
Yaron Pdut <yaron.pdut@sap.com>
2019-08-25 08:20:12 +0300

    initial app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoinsController } from './coins/coins.controller';
-import { CoinsService } from './coins/coins.service';
 import { CachettlController } from './cachettl/cachettl.controller';
-import { CachettlService } from './cachettl/cachettl.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoinsController, CachettlController],
-  providers: [AppService, CoinsService, CachettlService],
+  providers: [AppService],
 })
 export class AppModule {}
